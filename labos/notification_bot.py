0de56f3cb4c33b41041610da9d367e6ce733615f
joctavio287 <joctavio287@gmail.com>
2023-01-09 16:19:49 -0300

    actualizando el mensaje de telegram

--- labos/notification_bot.py
+++ labos/notification_bot.py
@@ -1,6 +1,14 @@
 import telegram
-def notify_ending(message):
+def mensaje_tel(mensaje: str = 'Termino de correr el script'):
+    '''
+    Manda un mensaje de telegram al chat que tenes con el bot 'python_scripts'.
+
+    INPUT: 
+    mensaje: str: mensaje para mandar al bot.
+    '''
     token = '5448153732:AAGhKraJQquEqMfpD3cb4rnTcrKB6U1ViMA'
     chat_id = '1034347542'
     bot = telegram.Bot(token = token)
-    bot.sendMessage(chat_id = chat_id, text = message)
+    bot.sendMessage(chat_id = chat_id, text = mensaje)
+if __name__ == '__main__':
+    mensaje_tel(mensaje = 'Este es un mensje de prueba')
