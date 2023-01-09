ffd88ada93f68e535e6b51ae110189a8f73e1056
joctavio287 <joctavio287@gmail.com>
2023-01-09 17:41:27 -0300

    actualizacion toml

--- labos/notificacion_bot.py
+++ labos/notificacion_bot.py
@@ -1,4 +1,5 @@
-import telegram
+import requests
+
 def mensaje_tel(mensaje: str = 'Termino de correr el script'):
     '''
     Manda un mensaje de telegram al chat que tenes con el bot 'python_scripts'.
@@ -6,9 +7,22 @@ def mensaje_tel(mensaje: str = 'Termino de correr el script'):
     INPUT: 
     mensaje: str: mensaje para mandar al bot.
     '''
-    token = '5448153732:AAGhKraJQquEqMfpD3cb4rnTcrKB6U1ViMA'
+    api_token = '5448153732:AAGhKraJQquEqMfpD3cb4rnTcrKB6U1ViMA'
+    api_url = f'https://api.telegram.org/bot{api_token}/sendMessage'
+    # api_url = f'https://api.telegram.org/bot{api_token}/sendPhoto'
     chat_id = '1034347542'
-    bot = telegram.Bot(token = token)
-    bot.sendMessage(chat_id = chat_id, text = mensaje)
+    try:
+        response = requests.post(api_url, json={'chat_id': chat_id, 'text': mensaje})
+        print(response.text)
+    except Exception as e:
+        print(e)
+
 if __name__ == '__main__':
-    mensaje_tel(mensaje = 'Este es un mensje de prueba')
+    # Se saca de BotFather
+    # token = '5448153732:AAGhKraJQquEqMfpD3cb4rnTcrKB6U1ViMA'
+    
+    # How to get the chat_id
+    # url = f'https://api.telegram.org/bot{token}/getUpdates'
+    # print(requests.get(url).json())
+    # chat_id = 1034347542
+    mensaje_tel('Los mensajes funcionan')
