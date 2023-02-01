143c6fff46f1cc7e7ab1d3e2aee11a04a2ebc37d
joctavio287 <joctavio287@gmail.com>
2023-01-31 22:04:31 -0300

    cambios de instrumentacion

--- labos/notificacion_bot.py
+++ labos/notificacion_bot.py
@@ -1,15 +1,22 @@
 import requests
 
-def mensaje_tel(mensaje: str = 'Termino de correr el script'):
+def mensaje_tel(api_token:str, chat_id: str, mensaje: str = 'Termino de correr el script'):
     '''
     Manda un mensaje de telegram al chat que tenes con el bot 'python_scripts'.
+    
+    El API token se saca del chat con BotFather. Para obtener el ID del chat us
+    ar el siguiente código. Si no funciona borrar el chat con el bot, inicializ
+    ar de nuevo y probar nuevamente.
 
-    INPUT: 
+    >>url = f'https://api.telegram.org/bot{token}/getUpdates'
+    >>print(requests.get(url).json())
+    
+    INPUT:
+    api_token: str: token del bot creado.
+    chat_id: str: id del chat al que queres mandar el mensaje. 
     mensaje: str: mensaje para mandar al bot.
     '''
-    api_token = '5448153732:AAGhKraJQquEqMfpD3cb4rnTcrKB6U1ViMA'
     api_url = f'https://api.telegram.org/bot{api_token}/sendMessage'
-    chat_id = '1034347542'
     try:
         response = requests.post(api_url, json={'chat_id': chat_id, 'text': mensaje})
         print(response.text)
@@ -17,13 +24,16 @@ def mensaje_tel(mensaje: str = 'Termino de correr el script'):
         print(e)
 
 if __name__ == '__main__':
-    # Se saca de BotFather
-    # token = '5448153732:AAGhKraJQquEqMfpD3cb4rnTcrKB6U1ViMA'
-    
+    # El API token se saca de BotFather
     # How to get the chat_id
     # url = f'https://api.telegram.org/bot{token}/getUpdates'
     # print(requests.get(url).json())
-    # chat_id = 1034347542
+    
     
     # Prueba de si los mensajes funcionan
-    mensaje_tel('Los mensajes funcionan')
+    mensaje_tel(
+    api_token = '5448153732:AAGhKraJQquEqMfpD3cb4rnTcrKB6U1ViMA',
+    chat_id = '1034347542',
+    mensaje = 'Los mensajes funcionan'
+    )
+    
